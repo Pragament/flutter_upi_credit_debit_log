89c5335ce533d5ccfbda24edab22b3431507443c
Harsh <harsh.vipinkp@gmail.com>
2024-08-27 23:31:48 +0530

    Add: status filter

--- lib/transaction_screen.dart
+++ lib/transaction_screen.dart
@@ -1,81 +1,29 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
-import 'package:image_picker/image_picker.dart';
-import 'package:hive/hive.dart';
-import 'pay.dart'; // Assuming your Hive models are in pay.dart
+import 'package:intl/intl.dart';
+import 'package:payment/pay.dart';
 
 class TransactionScreen extends StatefulWidget {
-  const TransactionScreen({Key? key}) : super(key: key);
+  const TransactionScreen({super.key});
 
   @override
+  // ignore: library_private_types_in_public_api
   _TransactionScreenState createState() => _TransactionScreenState();
 }
 
 class _TransactionScreenState extends State<TransactionScreen> {
-  final ImagePicker _picker = ImagePicker();
-  Box<Order>? orderBox;
-  List<Order> orders = [];
-  String? selectedAccount;
-  String selectedStatus = 'All';
-
-  @override
-  void initState() {
-    super.initState();
-    _openBox();
-  }
-
-  Future<void> _openBox() async {
-    try {
-      orderBox = await Hive.openBox<Order>('orders');
-      setState(() {
-        orders = orderBox!.values.toList();
-      });
-    } catch (error) {
-      print('Error opening Hive box: $error');
-    }
-  }
+  String _selectedStatus = 'all'; // Default to 'all'
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Recent Transactions'),
-      ),
-      body: orderBox == null
-          ? const Center(child: CircularProgressIndicator())
-          : Column(
-              children: [
-                _buildFilters(),
-                Expanded(
-                  child: orders.isEmpty
-                      ? const Center(child: Text('No transactions available'))
-                      : ListView.builder(
-                          itemCount: orders.length,
-                          itemBuilder: (context, index) {
-                            final order = orders[index];
-                            if (selectedStatus != 'All' &&
-                                order.status != selectedStatus) {
-                              return SizedBox.shrink();
-                            }
-                            return _buildOrderCard(order);
-                          },
-                        ),
-                ),
-              ],
-            ),
-    );
-  }
-
-  Widget _buildFilters() {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
+        actions: [
           DropdownButton<String>(
-            value: selectedStatus,
-            items: <String>['All', 'Pending', 'Completed'].map((String value) {
+            value: _selectedStatus,
+            items: <String>['all', 'pending', 'completed'].map((String value) {
               return DropdownMenuItem<String>(
                 value: value,
                 child: Text(value),
@@ -83,83 +31,101 @@ class _TransactionScreenState extends State<TransactionScreen> {
             }).toList(),
             onChanged: (String? newValue) {
               setState(() {
-                selectedStatus = newValue!;
+                _selectedStatus = newValue ?? 'all';
               });
             },
+            icon: const Icon(Icons.filter_list),
           ),
-          // Add account filter if needed
-          // DropdownButton<String>(
-          //   value: selectedAccount,
-          //   items: <String>['Account1', 'Account2'].map((String value) {
-          //     return DropdownMenuItem<String>(
-          //       value: value,
-          //       child: Text(value),
-          //     );
-          //   }).toList(),
-          //   onChanged: (String? newValue) {
-          //     setState(() {
-          //       selectedAccount = newValue;
-          //     });
-          //   },
-          // ),
         ],
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box<Order>('orders').listenable(),
+        builder: (context, Box<Order> box, _) {
+          final orders = box.values.where((order) {
+            if (_selectedStatus == 'all') return true;
+            return order.status == _selectedStatus;
+          }).toList();
+
+          if (orders.isEmpty) {
+            return const Center(
+              child: Text('No transactions available.'),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: orders.length,
+            itemBuilder: (context, index) {
+              final order = orders[index];
+              return OrderCard(order: order);
+            },
+          );
+        },
       ),
     );
   }
+}
 
-  Widget _buildOrderCard(Order order) {
+class OrderCard extends StatelessWidget {
+  final Order order;
+
+  const OrderCard({Key? key, required this.order}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final formattedTimestamp =
+        DateFormat('d MMMM yyyy, h:mma').format(order.timestamp);
+
     return Card(
-      margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      margin: const EdgeInsets.all(8.0),
       child: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Text('Order ID: ${order.orderId}',
-                style: Theme.of(context).textTheme.titleLarge),
-            Text('Amount: ${order.amount}',
-                style: Theme.of(context).textTheme.bodyLarge),
-            Text('Status: ${order.status}',
-                style: Theme.of(context).textTheme.bodyLarge),
-            Text('UTR Number: ${order.utrNumber}',
-                style: Theme.of(context).textTheme.bodyLarge),
-            Text('Client Notes: ${order.clientNotes}',
-                style: Theme.of(context).textTheme.bodyLarge),
-            SizedBox(height: 8),
-            Text('Invoice Image:',
-                style: Theme.of(context).textTheme.bodyLarge),
-            _buildImageWidget(order.invoiceImageUrl, () async {
-              final pickedFile =
-                  await _picker.pickImage(source: ImageSource.gallery);
-              if (pickedFile != null) {
-                setState(() {
-                  order.invoiceImageUrl = pickedFile.path;
-                  order.save();
-                });
-              }
-            }),
-            SizedBox(height: 8),
-            Text('Transaction Image:',
-                style: Theme.of(context).textTheme.bodyLarge),
-            _buildImageWidget(order.transactionImageUrl, () async {
-              final pickedFile =
-                  await _picker.pickImage(source: ImageSource.gallery);
-              if (pickedFile != null) {
-                setState(() {
-                  order.transactionImageUrl = pickedFile.path;
-                  order.save();
-                });
-              }
-            }),
-            SizedBox(height: 8),
-            Text('Timestamp: ${order.timestamp}',
-                style: Theme.of(context).textTheme.bodyLarge),
-            SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () {
-                _editOrder(order);
-              },
-              child: Text('Edit'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: ProductImagesList(productIds: order.products.keys.toList()),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.edit),
+                  onPressed: () {
+                    _showEditDialog(context, order);
+                  },
+                ),
+              ],
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Order ${order.status}',
+              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 8),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Placed at: $formattedTimestamp',
+                  style: const TextStyle(fontSize: 12),
+                ),
+                Text.rich(
+                  TextSpan(
+                    children: [
+                      const WidgetSpan(
+                        child: Icon(Icons.currency_rupee, size: 16),
+                      ),
+                      TextSpan(
+                        text: ' ${order.amount}',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
           ],
         ),
@@ -167,140 +133,106 @@ class _TransactionScreenState extends State<TransactionScreen> {
     );
   }
 
-  Widget _buildImageWidget(String? imagePath, Function onTap) {
-    return GestureDetector(
-      onTap: () => onTap(),
-      child: imagePath == null || imagePath.isEmpty
-          ? Container(
-              width: 100,
-              height: 100,
-              color: Colors.grey[300],
-              child: Icon(Icons.image, color: Colors.grey[700]),
-            )
-          : Image.file(File(imagePath), width: 100, height: 100),
-    );
-  }
-
-  void _editOrder(Order order) {
-    String newAmount = order.amount;
-    String newStatus = order.status;
-    String newUtrNumber = order.utrNumber;
-    String newClientNotes = order.clientNotes;
-
+  void _showEditDialog(BuildContext context, Order order) {
+    final _statusController = TextEditingController(text: order.status);
+    final _amountController = TextEditingController(text: order.amount);
+    String? _selectedStatus = order.status;
+    
     showDialog(
       context: context,
-      builder: (context) {
-        return StatefulBuilder(
-          builder: (context, setState) {
-            return AlertDialog(
-              title: Text('Edit Order'),
-              content: SingleChildScrollView(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    TextFormField(
-                      initialValue: order.amount,
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      onChanged: (value) => newAmount = value,
-                    ),
-                    DropdownButton<String>(
-                      value: newStatus,
-                      items:
-                          <String>['Pending', 'Completed'].map((String value) {
-                        return DropdownMenuItem<String>(
-                          value: value,
-                          child: Text(value),
-                        );
-                      }).toList(),
-                      onChanged: (String? newValue) {
-                        setState(() {
-                          newStatus = newValue!;
-                        });
-                      },
-                    ),
-                    TextFormField(
-                      initialValue: order.utrNumber,
-                      decoration: InputDecoration(labelText: 'UTR Number'),
-                      onChanged: (value) => newUtrNumber = value,
-                    ),
-                    TextFormField(
-                      initialValue: order.clientNotes,
-                      decoration: InputDecoration(labelText: 'Client Notes'),
-                      onChanged: (value) => newClientNotes = value,
-                    ),
-                    SizedBox(height: 16),
-                    Text('Invoice Image:'),
-                    SizedBox(height: 8),
-                    _buildImageWidget(order.invoiceImageUrl, () async {
-                      final pickedFile =
-                          await _picker.pickImage(source: ImageSource.gallery);
-                      if (pickedFile != null) {
-                        setState(() {
-                          order.invoiceImageUrl = pickedFile.path;
-                        });
-                      }
-                    }),
-                    SizedBox(height: 16),
-                    Text('Transaction Image:'),
-                    SizedBox(height: 8),
-                    _buildImageWidget(order.transactionImageUrl, () async {
-                      final pickedFile =
-                          await _picker.pickImage(source: ImageSource.gallery);
-                      if (pickedFile != null) {
-                        setState(() {
-                          order.transactionImageUrl = pickedFile.path;
-                        });
-                      }
-                    }),
-                  ],
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Edit Order'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _amountController,
+                decoration: const InputDecoration(
+                  labelText: 'Amount',
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 16),
+              DropdownButtonFormField<String>(
+                value: _selectedStatus,
+                items: <String>['pending', 'completed'].map((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+                onChanged: (String? newValue) {
+                  _selectedStatus = newValue;
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Status',
                 ),
               ),
-              actions: [
-                TextButton(
-                  onPressed: () {
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Cancel'),
-                ),
-                TextButton(
-                  onPressed: () async {
-                    await _updateOrder(
-                      order,
-                      newAmount,
-                      newStatus,
-                      newUtrNumber,
-                      newClientNotes,
-                    );
-                    Navigator.of(context).pop();
-                  },
-                  child: Text('Save'),
-                ),
-              ],
-            );
-          },
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                // Update the order with new data
+                if (_selectedStatus != null) {
+                  order.status = _selectedStatus!;
+                  order.amount = _amountController.text;
+                  order.save(); // Save changes to the Hive database
+                }
+
+                Navigator.pop(context);
+              },
+              child: const Text('Save Changes'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); // Close the dialog
+              },
+              child: const Text('Cancel'),
+            ),
+          ],
         );
       },
     );
   }
+}
 
-  Future<void> _updateOrder(
-    Order order,
-    String newAmount,
-    String newStatus,
-    String newUtrNumber,
-    String newClientNotes,
-  ) async {
-    // Update text fields
-    order.amount = newAmount;
-    order.status = newStatus;
-    order.utrNumber = newUtrNumber;
-    order.clientNotes = newClientNotes;
+class ProductImagesList extends StatelessWidget {
+  final List<int> productIds;
 
-    // Save the updated order back to Hive
-    await order.save();
+  const ProductImagesList({Key? key, required this.productIds})
+      : super(key: key);
 
-    // Refresh UI
-    setState(() {});
+  @override
+  Widget build(BuildContext context) {
+    final productBox = Hive.box<Product>('products');
+
+    return SizedBox(
+      height: 60,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: productIds.length,
+        itemBuilder: (context, index) {
+          final product = productBox.get(productIds[index]);
+
+          return product != null
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Column(
+                    children: [
+                      Image.file(
+                        File(product.imageUrl),
+                        width: 60,
+                        height: 60,
+                        fit: BoxFit.cover,
+                      ),
+                    ],
+                  ),
+                )
+              : const SizedBox();
+        },
+      ),
+    );
   }
 }
